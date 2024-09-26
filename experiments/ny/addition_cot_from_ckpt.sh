c7f43bcad6939ece634832221e74c37aed98151f
nayoung <nayoungl95@gmail.com>
2024-09-26 20:46:46 +0000

    experimenting dpo

---
+++ experiments/ny/addition_cot_from_ckpt.sh
@@ -0,0 +1,42 @@
+set -e
+
+# start from 1-10 digit CoT trained model and further train on 11-14 digits
+
+CUDA_VISIBLE_DEVICES=1 WANDB_PROJECT=arithmetic-dpo WANDB_MODE=online python run.py \
+    --architecture=llama \
+    --hidden_size=384 \
+    --num_attention_heads=6 \
+    --num_layers=6 \
+    --max_position_embeddings=1024 \
+    \
+    \
+    --n_digits_train='11,14' \
+    --num_train=50000 \
+    --op_train='add' \
+    --format_train='COT' \
+    --n_digits_eval='2,17,2' \
+    --op_eval='add' \
+    --format_eval='COT' \
+    \
+    \
+    --run_name='test' \
+    --output_dir=out \
+    --resume_from_checkpoint='out/test-llama-384-6-6-1024-COT-digits-1_10_/checkpoint-5000' \
+    --ignore_data_skip=True \
+    --do_train=True \
+    --do_eval=True \
+    --max_steps=6000 \
+    --learning_rate=1e-4 \
+    --lr_scheduler_type='cosine' \
+    --warmup_ratio=0.05 \
+    --logging_steps=20 \
+    --eval_strategy="steps" \
+    --eval_steps=200 \
+    --predict_with_generate \
+    --per_device_train_batch_size=256 \
+    --per_device_eval_batch_size=100 \
+    --gradient_accumulation_steps=2 \
+    --include_inputs_for_metrics=True \
+    --bf16=True \
+    --tf32=True \
+    --torch_compile=True
